4fbb66aed217ea3af2072d35e7de1fb236e0ae38
jagz71 <jgarcia.jag@gmail.com>
2023-05-15 10:13:37 -0400

    added some more comment. don't run workflow [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
+// Other comments
 
 describe('MainContent', () => {
   it('should render a button', () => {
